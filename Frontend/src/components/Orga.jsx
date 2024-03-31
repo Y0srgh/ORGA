a946286c74f21408064582852b38575c665b327f
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-03-31 13:11:37 +0100

    added the logo file

---
+++ Frontend/src/components/Orga.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Orga = () => {
+  return (
+    <div className='orga-logo'>
+        <h1 className='logo'>
+            ORGA
+        </h1>
+        <p className='slogan'>
+        Organisation  et gestion des ressources associatives
+        </p>
+    </div>
+  )
+}
+
+export default Orga
