06c87db42ff94de07deac778c8e057473f22c2f4
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-03-31 12:38:17 +0100

    fixed box

--- Frontend/src/components/signup/SignupForm.jsx
+++ Frontend/src/components/signup/SignupForm.jsx
@@ -4,7 +4,7 @@ import "./SignupFrom.css";
 const SignupForm = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedType, setSelectedType] = useState("your type");
-  const types = ["your type", "Admin", "Devure", "Pres"];
+  const types = ["Admin", "Devure", "Pres"];
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
