b04cc19e50023a441677005cdcfcc314e12153ae
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-03-31 10:06:31 +0100

    added the other fields

--- Frontend/src/components/signup/SignupForm.jsx
+++ Frontend/src/components/signup/SignupForm.jsx
@@ -11,8 +11,21 @@ const SignupForm = () => {
            */}
           <div className="input-box">
             <label>Prénom & Nom</label>
-            <input type="text" required placeholder="Username" />
+            <input type="text" required placeholder="Prénom  Nom" />
           </div>
+          <div className="input-box">
+            <label>Adresse Email</label>
+            <input type="email" required placeholder="prenom.nom@insat.ucar.tn" />
+          </div>
+          <div className="input-box">
+            <label>Numéro de téléphone</label>
+            <input type="tel" required placeholder="Votre numéro" />
+          </div>
+          <div className="input-box">
+            <label>Mot de passe</label>
+            <input type="password" required placeholder="Votre mot de passe" />
+          </div>
+
           
         </form>
       </div>
