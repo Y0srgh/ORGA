916153820ef2b2cb7898a831ee22176b7d1b8696
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-03-31 12:39:35 +0100

    adjusted the names of the items

--- Frontend/src/components/signup/SignupForm.jsx
+++ Frontend/src/components/signup/SignupForm.jsx
@@ -4,7 +4,7 @@ import "./SignupFrom.css";
 const SignupForm = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedType, setSelectedType] = useState("your type");
-  const types = ["Admin", "Devure", "Pres"];
+  const types = ["Admin", "Devure", "Président"];
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
