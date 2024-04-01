c5ffc5bec7e7e8d7894199932e773ef4c4bc31e0
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-04-01 10:42:59 +0100

    updated config.js

--- Backend/Configurations/config.js
+++ Backend/Configurations/config.js
@@ -1,5 +1,5 @@
 // Définition de la constante PORT avec la valeur 5500
 export const PORT =5500;
 // Définition de la constante mongoDBURL contenant l'URL de la base de données sur MongoDB Atlas
-export const mongoDBURL='mongodb://localhost:27017/orga'
-//'mongodb+srv://orgaAdmin:Fss4xpv1GWOPkvyc@clusterorga.hsvfrao.mongodb.net/ORGA?retryWrites=true&w=majority&appName=ClusterOrga'
+export const mongoDBURL=
+'mongodb+srv://orgaAdmin:Fss4xpv1GWOPkvyc@clusterorga.hsvfrao.mongodb.net/ORGA?retryWrites=true&w=majority&appName=ClusterOrga'
