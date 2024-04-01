8c86948fb08bfc77010b267442785b807cde3923
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-04-01 05:35:16 +0100

    commented the new check

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -5,32 +5,30 @@ export const addUser = async (req, res) => {
   try {
     // Destructure request body to extract user data
     const {
-        firstName,
-        lastName,
-        email,
-        password,
-        phoneNumber,
-        role,
-        levelOfStudy,
-        StudentID,
-        clubs
-      } =req.body;
-      
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      role,
+      levelOfStudy,
+      StudentID,
+      clubs,
+    } = req.body;
+
     // Check if all required fields are provided
 
     if (
-        
-            !firstName ||
-            !lastName ||
-            !email ||
-            !password ||
-            //
-            !phoneNumber ||
-            !role 
-            //
-            //
-          
-    ){
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      //
+      !phoneNumber ||
+      !role
+      //
+      //
+    ) {
       return res
         .status(400)
         .json({ message: "Veuillez fournir tous les champs requis." });
@@ -40,15 +38,21 @@ export const addUser = async (req, res) => {
     if (role === "Président" && (!levelOfStudy || !StudentID || !clubs)) {
       return res
         .status(400)
-        .json({ message: "Veuillez fournir tous les champs requis pour le rôle de Président." });
+        .json({
+          message:
+            "Veuillez fournir tous les champs requis pour le rôle de Président.",
+        });
     }
 
+    // Check if role is not "Président" but student-related fields are provided
     if (role === "Dvure" && (levelOfStudy || StudentID || clubs)) {
       return res
         .status(400)
-        .json({ message: "Vous ne pouvez pas ajouter des informations spécifiques aux étudiants." });
+        .json({
+          message:
+            "Vous ne pouvez pas ajouter des informations spécifiques aux étudiants.",
+        });
     }
-
 
     // Check if user with the provided phone number already exists
 
@@ -56,25 +60,26 @@ export const addUser = async (req, res) => {
     if (existingUser) {
       return res
         .status(409)
-        .json({ message: "Un utilisateur avec ce numéro de téléphone existe déjà." });
+        .json({
+          message: "Un utilisateur avec ce numéro de téléphone existe déjà.",
+        });
     }
     // Creating a new user in the database
 
     const newUser = await User.create({
-        firstName,
-        lastName,
-        email,
-        password,
-        phoneNumber,
-        role,
-        levelOfStudy,
-        StudentID,
-        clubs
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      role,
+      levelOfStudy,
+      StudentID,
+      clubs,
     });
 
     return res.status(201).json(newUser);
-  } 
-  catch (error) {
+  } catch (error) {
     console.error("Erreur lors de l'ajout de l'utilisateur :", error);
     return res.status(500).json({
       message: "Une erreur est survenue lors de l'ajout de l'utilisateur.",
@@ -84,7 +89,7 @@ export const addUser = async (req, res) => {
 
 export const findAllUsers = async (req, res) => {
   try {
-        // Find all users in the database
+    // Find all users in the database
     const users = await User.find({});
     return res.status(200).json({
       count: users.length,
@@ -93,7 +98,8 @@ export const findAllUsers = async (req, res) => {
   } catch (error) {
     console.error("Erreur lors de la récupération des utilisateurs :", error);
     return res.status(500).json({
-      message: "Une erreur est survenue lors de la récupération des utilisateurs.",
+      message:
+        "Une erreur est survenue lors de la récupération des utilisateurs.",
     });
   }
 };
@@ -143,9 +149,9 @@ export const updatePassword = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const  {
+  try {
+    const { id } = req.params;
+    const {
       firstName,
       lastName,
       email,
@@ -154,55 +160,61 @@ export const updateUser = async (req, res) => {
       phoneNumber,
       role,
       StudentID,
-      clubs} = req.body;
-  
-      if ( 
-        
-        !firstName ||
-        !lastName ||
-        !email ||
-        !password ||
-        !levelOfStudy ||
-        !phoneNumber ||
-        !role ||
-        !StudentID ||
-        !clubs) {
-        return res.status(400).json({
-          message: 'Veuillez fournir tous les champs requis',
-        });
-      }
-  
-      let updatedFields = {
-      firstName,
-      lastName,
-      email,
-      password,
-      levelOfStudy,
-      phoneNumber,
-      role,
-      StudentID,
-      clubs
-      };
-        // If password is provided, hash it
-      if (password) {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        updatedFields.password = hashedPassword;
-      }
-  
-      const updatedUser = await User.findByIdAndUpdate(id, updatedFields, { new: true });
-  
-      if (!updatedUser) {
-        return res.status(404).json({ message: "Utilisateur non trouvé." });
-      }
-  
-      return res.status(200).json({ message: "Utilisateur mis à jour avec succès."});
-    } catch (error) {
-      console.error("Erreur lors de la mise à jour de l'utilisateur :", error);
-      return res.status(500).json({
-        message: "Une erreur est survenue lors de la mise à jour de l'utilisateur.",
+      clubs,
+    } = req.body;
+
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !levelOfStudy ||
+      !phoneNumber ||
+      !role ||
+      !StudentID ||
+      !clubs
+    ) {
+      return res.status(400).json({
+        message: "Veuillez fournir tous les champs requis",
       });
     }
-  };
+
+    let updatedFields = {
+      firstName,
+      lastName,
+      email,
+      password,
+      levelOfStudy,
+      phoneNumber,
+      role,
+      StudentID,
+      clubs,
+    };
+    // If password is provided, hash it
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      updatedFields.password = hashedPassword;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, updatedFields, {
+      new: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Utilisateur non trouvé." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Utilisateur mis à jour avec succès." });
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour de l'utilisateur :", error);
+    return res.status(500).json({
+      message:
+        "Une erreur est survenue lors de la mise à jour de l'utilisateur.",
+    });
+  }
+};
 
 export const deleteUser = async (req, res) => {
   try {
@@ -215,12 +227,11 @@ export const deleteUser = async (req, res) => {
       return res.status(404).json({ message: "Utilisateur non trouvé." });
     }
 
-    return res.status(200).json({ message: "Utilisateur supprimé avec succès." });
+    return res
+      .status(200)
+      .json({ message: "Utilisateur supprimé avec succès." });
   } catch (error) {
-    console.error(
-      "Erreur lors de la suppression de l'utilisateur :",
-      error
-    );
+    console.error("Erreur lors de la suppression de l'utilisateur :", error);
     return res.status(500).json({
       message:
         "Une erreur est survenue lors de la suppression de l'utilisateur.",
