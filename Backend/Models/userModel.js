c18ea4766c3904c4e4be581dcdda3472db6664c9
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-04-03 11:15:49 +0100

    included the salt in the usermodel

--- Backend/Models/userModel.js
+++ Backend/Models/userModel.js
@@ -63,7 +63,7 @@ userSchema.pre("save", async function (next) {
   const user = this;
   if (user.isModified("password")) {
     const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(user.password, 10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
     user.password = hashedPassword;
   }
   next();
