833c67f0204fb0d8f87016a1a52f17f85396060d
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-04-05 10:56:39 +0100

    added a second option for the "sign up"

---
+++ Frontend/src/components/signup/SignupForm-option2.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios, { AxiosError } from "axios";
+import { SnackbarProvider, useSnackbar } from "notistack";
+import "./SignupFrom.css";
+
+const SignupForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [role, setRole] = useState("");
+  const navigate = useNavigate();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedType, setSelectedType] = useState(
+    "Choisissez votre type de profil"
+  );
+  const types = ["Dvure", "Président"];
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const selectType = (type) => {
+    setSelectedType(type);
+    setIsOpen(false);
+    setRole(type); // Update role state with the selected type
+  };
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      role,
+    };
+    if (role === "Président") {
+      data.levelOfStudy = 1;
+      data.StudentID = "2100";
+    }
+    await axios
+      .post("http://localhost:5500/users", data)
+      .then(() => {
+        enqueueSnackbar('La demande a été enregistrée avec succès!', {
+          variant: 'success',
+        });
+        navigate("/signup");
+      })
+      .catch((error) => {
+        enqueueSnackbar(error.response.data.message, { variant: 'error' });
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <div className="wrapper">
+        <form onSubmit={handleSubmit}>
+          <h1>Bienvenue à bord !</h1>
+          <div className="input-box">
+            <div className="input-half">
+              <label>Prénom</label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+                placeholder="Votre Prénom"
+              />
+            </div>
+            <div className="input-half">
+              <label>Nom</label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+                placeholder="Votre Nom"
+              />
+            </div>
+          </div>
+          <div className="input-box">
+            <label>Adresse Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="prenom.nom@insat.ucar.tn"
+            />
+          </div>
+          <div className="input-box">
+            <label>Numéro de téléphone</label>
+            <input
+              type="tel"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              required
+              placeholder="Votre numéro"
+            />
+          </div>
+          <div className="input-box">
+            <label>Mot de passe</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Votre mot de passe"
+            />
+          </div>
+
+          <div className="input-box dropdown">
+            <div className="select" onClick={toggleDropdown}>
+              <span className="selected">{selectedType}</span>
+              <div className={`caret ${isOpen ? "caret-rotate" : ""}`}></div>
+            </div>
+            <ul className={`menu ${isOpen ? "menu-open" : ""}`}>
+              {types.map((type, index) => (
+                <li key={index} onClick={() => selectType(type)}>
+                  {type}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <button type="submit">S'inscrire</button>
+          <div className="login-link">
+            <p>
+              Avez-vous déjà un compte? <Link href="#">se connecter</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
