2f27d1c76abe35fb62c1bd5b48a41495fe30a07b
Y0srgh <yosr.ghozzi@insat.ucar.tn>
2024-04-12 18:37:47 +0100

    added the create club controller

--- Backend/Controllers/clubContoller.js
+++ Backend/Controllers/clubContoller.js
@@ -1,22 +1,40 @@
 import { Club } from "../Models/clubModel";
 
-export const findAllClubs = async (req, res) => { 
+export const findAllClubs = async (req, res) => {
   try {
     const clubs = await Club.find();
     return res.status(200).json({
-        count:clubs.length, 
-        data: clubs});
+      count: clubs.length,
+      data: clubs,
+    });
   } catch (error) {
     return res.status(404).json({ message: error.message });
   }
-}
+};
 
 export const findClubById = async (req, res) => {
-    try {
+  try {
     const club = await Club.findById(req.params.id);
     return res.status(200).json(club);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    return res.status(404).json({ message: error.message });
   }
-}
+};
 
+export const createClub = async (req, res) => {
+  try {
+    const { clubName } = req.body;
+
+    if (!clubName) {
+      return res.status(400).json({ message: "Club name is required" });
+    }
+    const existingClub = await Club.findOne({ clubName });
+    if (existingClub) {
+      return res.status(400).json({ message: "Club already exists" });
+    }
+    const club = await Club.create(req.body);
+    return res.status(201).json(club);
+  } catch (error) {
+    return res.status(409).json({ message: error.message });
+  }
+};
