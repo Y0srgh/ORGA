be9db5cc58c84adc79b74459e3cd9663b88babed
lamiabelhadj <lamia.belhadjsghaier@insat.ucar.tn>
2024-04-17 15:39:59 +0100

    fixed the way data is sent to  the last form

--- Frontend/src/Components/Reserver/ParentComponent.jsx
+++ Frontend/src/Components/Reserver/ParentComponent.jsx
@@ -8,15 +8,16 @@ function ParentComponent() {
   const [showReservationDetails, setShowReservationDetails] = useState(false);
   const [reservationDetails, setReservationDetails] = useState({ date: null, time: null, salle: null, motif: null });
 
-  const handleReservationTimeDateSubmit = () => {
+  const handleReservationTimeDateSubmit = (date, time) => {
     // Handle logic when ReservationTimeDate form is submitted
     setReserverTimeDateSubmitted(true);
+    setReservationDetails(prevState => ({ ...prevState, date, time }));
   };
 
-  const handleReservationSalleformSubmit = (date, time, salle, motif) => {
+  const handleReservationSalleformSubmit = (salle, motif) => {
     // Handle logic when ReservationSalleform form is submitted
     setShowReservationDetails(true); // Set state to show reservation details after salle form submission
-    setReservationDetails({ salle, motif ,date, time}); // Save the submitted date, time, salle, and motif
+    setReservationDetails(prevState => ({ ...prevState, salle, motif }));
   };
 
   return (
@@ -28,11 +29,11 @@ function ParentComponent() {
         <ReserverSalleform onSubmit={handleReservationSalleformSubmit} />
       )}
       {showReservationDetails && (
-        <ReservationDetails 
-          date={reservationDetails.date} 
-          time={reservationDetails.time} 
-          salle={reservationDetails.salle} 
-          motif={reservationDetails.motif} 
+        <ReservationDetails
+          date={reservationDetails.date}
+          time={reservationDetails.time}
+          salle={reservationDetails.salle}
+          motif={reservationDetails.motif}
         />
       )}
     </div>
