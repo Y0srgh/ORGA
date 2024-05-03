7a162fbb0a9f5b5fe5f43af830ad1948f49d4941
lamiabelhadj <123773640+lamiabelhadj@users.noreply.github.com>
2024-05-03 21:39:48 +0100

    Updated ReservationController.js

--- Backend/Controllers/ReservationController.js
+++ Backend/Controllers/ReservationController.js
@@ -29,4 +29,100 @@ export const addReservation = async (req, res) => {
   }
 };
 
+export const deleteReservation = async (req, res) => {
+  try {
+    const { id } = req.params;
 
+    const reservation = await Reservation.findByIdAndDelete(id);
+
+    if (!reservation) {
+      return res.status(404).json({ message: "Réservation non trouvée." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Réservation supprimée avec succès." });
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        "Une erreur est survenue lors de la suppression de la réservation.",
+    });
+  }
+};
+
+export const findAllReservations = async (req, res) => {
+  try {
+    const reservations = await Reservation.find({});
+    return res.status(200).json({
+      count: reservations.length,
+      data: reservations,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        "Une erreur est survenue lors de la récupération des réservations.",
+    });
+  }
+};
+
+export const findOneReservation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const reservation = await Reservation.findById(id);
+    return res.status(200).json(reservation);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+export const updateReservation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { salle, motif, date, time } = req.body;
+
+    const updatedReservation = await Reservation.findByIdAndUpdate(
+      id,
+      { salle, motif, date, time },
+      { new: true }
+    );
+
+    if (!updatedReservation) {
+      return res.status(404).json({ message: "Réservation non trouvée." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Réservation mise à jour avec succès." });
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        "Une erreur est survenue lors de la mise à jour de la réservation.",
+    });
+  }
+};
+
+export const updateState = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { state } = req.body;
+
+    const updatedReservation = await Reservation.findByIdAndUpdate(
+      id,
+      { state },
+      { new: true }
+    );
+
+    if (!updatedReservation) {
+      return res.status(404).json({ message: "Réservation non trouvée." });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "État de la réservation mis à jour avec succès." });
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        "Une erreur est survenue lors de la mise à jour de l'état de la réservation.",
+    });
+  }
+};
