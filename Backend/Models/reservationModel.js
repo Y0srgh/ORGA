ede1988a4297362b4c2928401df27671be312f9a
lamiabelhadj <123773640+lamiabelhadj@users.noreply.github.com>
2024-05-30 14:19:10 +0100

    Updated reservationModel.js

--- Backend/Models/reservationModel.js
+++ Backend/Models/reservationModel.js
@@ -14,26 +14,35 @@ const reservationSchema = mongoose.Schema(
       type: String,
       required: true, // Motive for the reservation
     },
+
+    facility: {
+      //type: mongoose.Schema.Types.ObjectId,
+      //ref: "Facility", // Reference to the facility collection
+      type: String,
+      required: true, // Reference to the facility for which the reservation is made
+    },
     state: {
       type: String,
       enum: ["En attente", "Approuvée", "Refusée" ,"Annulée"], // State of the reservation
       default: "En attente", // Default state is pending
     },
-    facility: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Facility", // Reference to the facility collection
-      required: true, // Reference to the facility for which the reservation is made
+    club: {
+      type: String,
+ 
     },
-    userId: {
+  
+   userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true, // Reference to the user who made the reservation
+      required: false, // Reference to the user who made the reservation
     },
-    adminId: {
+    
+  
+    /*adminId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         default: null, 
-      },
+      },*/
   },
   {
     timestamps: true,
