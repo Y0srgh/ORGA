f678f6933a3d1ff133f07210fba3cf667aed3d81
lamiabelhadj <123773640+lamiabelhadj@users.noreply.github.com>
2024-05-31 02:03:54 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,53 +1,45 @@
+
+
+
 import mongoose from "mongoose"; // Import Mongoose for MongoDB interaction
-// Importer l'URL de MongoDB et la configuration du port
 import { mongoDBURL, PORT } from "./Configurations/config.js"; 
-import  cors  from "cors";
+import cors from "cors";
 import userRoutes from "./Routes/userRoutes.js";
 import reservationRoutes from "./Routes/reservationRoutes.js";
 import facilityRoutes from "./Routes/facilityRoutes.js";
 import cookieParser from "cookie-parser";
-// Import Express framework
 import express from "express"; 
-export const app = express(); 
 
-// Middleware pour l'analyse du corps de la requête (parsing request body)
+const app = express(); 
+
+// Middleware for parsing request body
 app.use(express.json());
-app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        credentials:true,
-    })
-);
-// Mount userRoutes middleware at the '/users' path
-app.use('/reservations',reservationRoutes);
-app.use('/users',userRoutes);
-app.use('/facilities',facilityRoutes);
-
-
-// Middleware for handling CORS POLICY
-// option 1: Allow All Origins with Default of cors(*)
-//app.use(cors());
-
-
-
-// Option 2: Allow Custom Origins
-/*app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-)*/
-// Middleware for parsing cookies
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Credentials', true); // Allow cookies
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(cookieParser());
-// Se connecter à la base de données MongoDB Atlas
+
+// Mounting routes
+app.use('/reservations', reservationRoutes);
+app.use('/users', userRoutes);
+app.use('/facilities', facilityRoutes);
+
+// Connect to MongoDB Atlas
 mongoose.connect(mongoDBURL)
     .then(() => {
         console.log("App connected to database");
-
-        // Start Express server to listen for incoming requests on specified port
         app.listen(PORT, () => console.log(`Server running on port ${PORT} 🔥`));
     })
     .catch((error) => {
         console.error(error);
     });
+
+export default app;
